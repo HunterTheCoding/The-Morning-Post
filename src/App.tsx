2f0bda096b525bbc060e7cd1f832e6d311e2836d
Hassan Ali <hassan4np@gmail.com>
2024-01-16 18:14:48 +0600

    my first project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1 className='text-red-700 font-bold'>Vite + React</h1>
+    
+    </>
+  )
+}
+
+export default App
