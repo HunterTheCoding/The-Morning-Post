a5e5154b412113bf9dcb7f34a03003fd48a75985
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-01-17 17:02:43 +0600

    start a Navber Section

--- src/Components/Navbar/Footer.tsx
+++ src/Components/Navbar/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
     return (
         <div>
-            <h1>footer</h1>
+            
         </div>
     );
 };
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,8 +1,55 @@
+import Marquee from "react-fast-marquee";
+import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>navbar</h1>
+        <div className="div-h">
+            <header>
+                <h1>Your News Website</h1>
+                <img src="https://via.placeholder.com/100" alt="logo" />
+            </header>
+
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">World</a>
+                <a href="#">Politics</a>
+                <a href="#">Business
+                    <ul>
+                        <li><a href="#">business 1</a></li>
+                        <li><a href="#">business 2</a></li>
+                        <li><a href="#">business 3</a></li>
+                    </ul>
+                </a>
+                <a href="#">Sports
+                    <ul>
+                        <li><a href="#">Football</a></li>
+                        <li><a href="#">Basketball</a></li>
+                        <li><a href="#">Baseball</a></li>
+                    </ul>
+                </a>
+                <a href="#">Entertainment
+                    <ul>
+                        <li><a href="#">Entertainment 1</a></li>
+                        <li><a href="#">Entertainment 2</a></li>
+                        <li><a href="#">Entertainment 3</a></li>
+                    </ul>
+                </a>
+                <a href="#">Technology</a>
+                <a href="#">Opinion
+                    <ul>
+                        <li><a href="#">Opinion 1</a></li>
+                        <li><a href="#">Opinion 2</a></li>
+                        <li><a href="#">Opinion 3</a></li>
+                    </ul>
+                </a>
+            </nav>
+            <div className="text-black flex">
+                Breaking News:
+                <Marquee>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et lectus nec turpis consequat
+                    posuere.
+                </Marquee>
+            </div>
         </div>
     );
 };
