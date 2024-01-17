b16b86af9b295d905738bf32d27479d7f7fa2c64
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-01-17 19:38:25 +0600

    complete a footer section simple design

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,17 +4,22 @@ const Navbar = () => {
     return (
         <div className="font-sans bg-gray-300 m-0 p-0">
             <header className="bg-gray-800 text-white p-6 text-center flex justify-between px-5vw">
-                <h1>Your News Website</h1>
+                <div className="flex flex-col text-start">
+                    <div>
+                        <p>We are for the people</p>
+                    </div>
+                    <h1 className=" ml-10 font-extrabold text-4xl">The Morning Post</h1>
+                </div>
                 <img src="https://via.placeholder.com/100" alt="logo" />
             </header>
 
             <nav className="bg-gray-700 overflow-hidden pl-20 px-5vw">
                 <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700" href="#">Home</a>
-                <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700"href="#">World</a>
+                <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700" href="#">World</a>
                 <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700" href="#">Politics</a>
                 <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700" href="#">Business</a>
                 <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700" href="#">Sports</a>
-                <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700"href="#">Entertainment</a>
+                <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700" href="#">Entertainment</a>
                 <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700" href="#">Technology</a>
                 <a className="block float-left text-white text-center border-r-2 border-gray-300 no-underline relative py-3 px-4 hover:bg-gray-300 hover:text-gray-700" href="#">Opinion</a>
             </nav>
