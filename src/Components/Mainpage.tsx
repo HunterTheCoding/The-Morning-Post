8af0ef7169cbac44e2d1eca39c6226f20a1f211e
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-02-22 01:11:49 +0600

    Live button add

--- src/Components/Mainpage.tsx
+++ src/Components/Mainpage.tsx
@@ -6,7 +6,7 @@ import Footer from "./Navbar/Footer";
 const Mainpage = () => {
     return (
         <div>
-            <div className="... sticky top-0">A</div>
+            <div className="... sticky top-0"><button className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 btn-sm font-medium rounded-lg text-sm px-5 text-center me-2 mb-2">Live</button></div>
             <div>
                 <Navbar></Navbar>
                 <Outlet></Outlet>
