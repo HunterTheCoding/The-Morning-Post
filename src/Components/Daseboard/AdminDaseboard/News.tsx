5298bbf12165bd0d7619295489b00a9a408499a6
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-22 09:01:08 +0600

    integrate with react hook form

--- src/Components/Daseboard/AdminDaseboard/News.tsx
+++ src/Components/Daseboard/AdminDaseboard/News.tsx
@@ -1,12 +1,15 @@
-
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const News = () => {
+
+    const { register, handleSubmit, reset } = useForm();
+
     return (
         <div>
             <h1 className="text-center font-bold my-4 text-4xl ">Post News</h1>
             <div className="bg-gray-200 p-4 rounded-lg m-4">
                 <form>
-
+                    
                 </form>
             </div>
         </div>
