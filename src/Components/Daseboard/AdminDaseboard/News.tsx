adeba0b2a12ad6573c5ab9be7390d0ab994798de
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-22 09:07:34 +0600

    added onsubmit with form

--- src/Components/Daseboard/AdminDaseboard/News.tsx
+++ src/Components/Daseboard/AdminDaseboard/News.tsx
@@ -4,11 +4,15 @@ const News = () => {
 
     const { register, handleSubmit, reset } = useForm();
 
+    const onSubmit: SubmitHandler<Inputs> = async (data) =>{
+        console.log(data);
+    }
+
     return (
         <div>
             <h1 className="text-center font-bold my-4 text-4xl ">Post News</h1>
             <div className="bg-gray-200 p-4 rounded-lg m-4">
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     
                 </form>
             </div>
