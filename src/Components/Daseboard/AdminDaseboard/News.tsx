005804fa971e0a440277759876eb4c6b5936bc5b
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-22 08:56:43 +0600

    added a form into a div

--- src/Components/Daseboard/AdminDaseboard/News.tsx
+++ src/Components/Daseboard/AdminDaseboard/News.tsx
@@ -3,7 +3,12 @@
 const News = () => {
     return (
         <div>
-            
+            <h1 className="text-center font-bold my-4 text-4xl ">Post News</h1>
+            <div className="bg-gray-200 p-4 rounded-lg m-4">
+                <form>
+
+                </form>
+            </div>
         </div>
     );
 };
