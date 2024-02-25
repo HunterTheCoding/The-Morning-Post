abf7b4f26bac5fe7b07681440bdc63462a174fd2
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-02-25 17:21:55 +0600

    live section data feching problem solve

--- src/Hook/useAxiosPublic.ts
+++ src/Hook/useAxiosPublic.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 interface axiosPublic extends AxiosInstance { }
 const AxiosPublic: axiosPublic = axios.create({
-  // baseURL: "https://the-mornong-post-server-omega.vercel.app",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://the-mornong-post-server-omega.vercel.app",
+  // baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 const useAxiosPublic = () => {
