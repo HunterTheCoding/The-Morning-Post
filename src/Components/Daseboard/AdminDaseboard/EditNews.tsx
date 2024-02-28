f5271c26358c00f532a93fad16be29db5a6276f2
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-28 13:25:41 +0600

    integrated with react hook form

--- src/Components/Daseboard/AdminDaseboard/EditNews.tsx
+++ src/Components/Daseboard/AdminDaseboard/EditNews.tsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import useSingleNews from "../../../Hook/useSingleNews";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 
 
 const EditNews: React.FC = () => {
@@ -8,11 +10,17 @@ const EditNews: React.FC = () => {
     const { news } = useSingleNews(id);
     console.log(news);
 
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit: SubmitHandler = async(data) =>{
+        console.log(data);
+    }
+
     return (
         <div>
             <h1 className="text-center font-bold my-4 text-4xl ">Update News</h1>
             <div className="bg-gray-200 p-4 rounded-lg m-4">
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     
                 </form>
             </div>
