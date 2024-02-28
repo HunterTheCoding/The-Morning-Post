065d30651355e5859b1c98ed07020d5ee0854a95
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-28 13:16:33 +0600

    made a form

--- src/Components/Daseboard/AdminDaseboard/EditNews.tsx
+++ src/Components/Daseboard/AdminDaseboard/EditNews.tsx
@@ -10,7 +10,12 @@ const EditNews: React.FC = () => {
 
     return (
         <div>
-
+            <h1 className="text-center font-bold my-4 text-4xl ">Update News</h1>
+            <div className="bg-gray-200 p-4 rounded-lg m-4">
+                <form>
+                    
+                </form>
+            </div>
         </div>
     );
 };
