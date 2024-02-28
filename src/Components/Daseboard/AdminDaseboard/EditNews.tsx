c8b8dda40acde6ebc62ca96040583a86dc4ea40a
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-28 16:03:22 +0600

    added input for section

--- src/Components/Daseboard/AdminDaseboard/EditNews.tsx
+++ src/Components/Daseboard/AdminDaseboard/EditNews.tsx
@@ -31,7 +31,43 @@ const EditNews: React.FC = () => {
             <h1 className="text-center font-bold my-4 text-4xl ">Update News</h1>
             <div className="bg-gray-200 p-4 rounded-lg m-4">
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    
+                <div className="flex gap-6">
+                        {/* section */}
+                        <div className="form-control md:w-1/2 w-full my-6">
+                            <label className="label">
+                                <span className="label-text text-red-600 font-bold">Section*</span>
+
+                            </label>
+
+                            
+                            <select defaultValue='default' {...register("section", { required: true })} required className="select select-bordered w-full mr-4">
+                                <option disabled value='default'>Select a section</option>
+                                <option value="National">National</option>
+                                <option value="International">International</option>
+                                <option value="Politics">Politics</option>
+                                <option value="Business">Business</option>
+                                <option value="Education">Education</option>
+                                <option value="Food">Food</option>
+                                <option value="HotLight">HotLight</option>
+                                <option value="Health">Health</option>
+                                <option value="Science">Science</option>
+                                <option value="Technology">Technology</option>
+                                <option value="Feature">Feature</option>
+                                <option value="Sports">Sports</option>
+                                <option value="Music">Music</option>
+                                <option value="Environment">Environment</option>
+                                <option value="Travel">Travel</option>
+                                <option value="Gaming">Gaming</option>
+                                <option value="Fashion">Fashion</option>
+                                <option value="Automotive">Automotive</option>
+                                <option value="Literature">Literature</option>
+                                <option value="Entertainment">Entertainment</option>
+                                <option value="history">History</option>
+                                <option value="Literature">Literature</option>
+                            </select>
+                        </div>
+                        
+                    </div>
                 </form>
             </div>
         </div>
