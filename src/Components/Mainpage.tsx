7e2d5057895d28ac1fb83df058ce6f6d607a7d0f
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-02-29 22:42:27 +0600

    notification update

--- src/Components/Mainpage.tsx
+++ src/Components/Mainpage.tsx
@@ -4,35 +4,31 @@ import Footer from "./Navbar/Footer";
 import useAxiosPublic from "../Hook/useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
+import { getToken } from "@firebase/messaging";
+import { messaging } from "./Firbase/Firebase.config";
+
 
 
 
 
 const Mainpage = () => {
-    function requestNotificationPermission() {
-        return new Promise((resolve, reject) => {
-          Notification.requestPermission().then(permission => {
-            resolve(permission);
-          }).catch(error => {
-            reject(error);
-          });
-        });
-      }
-      
-      // Example usage
-      requestNotificationPermission().then(permission => {
-        if (permission === 'granted') {
-          // Permission granted, proceed with notifications
-        } else {
-          // Handle other cases (denied or default)
+    async function requestPermission() {
+        const permission = await Notification.requestPermission();
+        if (permission === "granted") {
+          // Generate Token
+          const token =await getToken(messaging, { vapidKey: "BCLdO29NdIfiWxYW8GcKcjL3_z3-VyT88zf1L66EtOPVzQC_vTkvRU5HBJaLHCjAeA0jDu2XnhGvqVgcU7nxPG0" })
+          console.log(token);  
+          // Send this token  to server ( db)
+        } else if (permission === "denied") {
+          alert("You denied for the notification");
         }
-      }).catch(error => {
-        console.error('Error requesting notification permission:', error);
-      });
-
+      }
+    
       useEffect(() => {
-        requestNotificationPermission();
+        // Req user for notification permission
+        requestPermission();
       }, []);
+    
 
     const AxiosPublic = useAxiosPublic();
     const { isPending, error, refetch, data: Live } = useQuery({
@@ -48,27 +44,27 @@ const Mainpage = () => {
 
     return (
         <div>
-            <div className="... sticky top-0">{Live.map((d: any) =><Link key={d?._id} to={d?.Link}><button className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 btn-sm font-medium rounded-lg text-sm px-5 text-center me-2 mb-2">Live</button></Link>)}</div>
+            <div className="... sticky top-0">{Live.map((d: any) => <Link key={d?._id} to={d?.Link}><button className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 btn-sm font-medium rounded-lg text-sm px-5 text-center me-2 mb-2">Live</button></Link>)}</div>
             <div>
                 <Navbar></Navbar>
                 <Outlet></Outlet>
                 <Footer></Footer>
             </div>
             <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+                <header className="App-header">
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
         </div>
     );
 };
