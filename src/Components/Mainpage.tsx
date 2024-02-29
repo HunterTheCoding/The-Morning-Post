b065cda259bb729def9de28890e683ac4d88d391
Hassan Ali <hassan4np@gmail.com>
2024-03-01 01:31:54 +0600

    update home page and all page design

--- src/Components/Mainpage.tsx
+++ src/Components/Mainpage.tsx
@@ -44,7 +44,7 @@ const Mainpage = () => {
 
     return (
         <div className="-mt-6">
-            <div className="... sticky top-0">{Live.map((d: any) => <Link key={d?._id} to={d?.Link}><button className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 btn-sm font-medium rounded-lg text-sm px-5 text-center me-2 mb-2">Live</button></Link>)}</div>
+            <div className="... sticky top-0 z-20">{Live.map((d: any) => <Link key={d?._id} to={d?.Link}><button className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 btn-sm font-medium rounded-lg text-sm px-5 text-center me-2 mb-2">Live</button></Link>)}</div>
             <div>
                 <Navbar></Navbar>
                 <Outlet></Outlet>
