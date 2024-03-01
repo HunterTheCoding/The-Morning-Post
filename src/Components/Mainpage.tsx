27ff1759f12f0a151295c5782b1388da99cc6554
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-03-01 14:02:51 +0600

    update resolve the bug and code update

--- src/Components/Mainpage.tsx
+++ src/Components/Mainpage.tsx
@@ -50,21 +50,6 @@ const Mainpage = () => {
                 <Outlet></Outlet>
                 <Footer></Footer>
             </div>
-            <div className="App">
-                <header className="App-header">
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
         </div>
     );
 };
